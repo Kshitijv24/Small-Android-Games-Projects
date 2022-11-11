85cdcc7e32d6b010cc1a579038cbd3def577dfeb
Kshitijv24 <kshitijv24@gmail.com>
2022-11-11 19:22:46 +0530

    now we can release the ball when we stop dragging the ball

--- Ball-Launcher/Assets/Scripts/BallHandler.cs
+++ Ball-Launcher/Assets/Scripts/BallHandler.cs
@@ -1,11 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] Rigidbody2D currentBallRigidbody;
+    [SerializeField] SpringJoint2D currentBallSpringJoint;
+    [SerializeField] float detachDelay;
 
     Camera mainCamera;
+    bool isDragging;
 
     private void Start()
     {
@@ -14,16 +18,38 @@ public class BallHandler : MonoBehaviour
 
     private void Update()
     {
+        if(currentBallRigidbody == null) { return; }
+
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            currentBallRigidbody.isKinematic = false;
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+
+            isDragging = false;
             return;
         }
 
+        isDragging = true;
         currentBallRigidbody.isKinematic = true;
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
         currentBallRigidbody.position = worldPosition;
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
